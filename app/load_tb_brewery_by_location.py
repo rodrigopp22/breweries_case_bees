2a870ff816beaa2875c58e1e0c189c0ec7ea9144
Rodrigo Pasquale <digoppasquale@msn.com>
2024-10-16 15:14:36 -0300

    Refatora código, ajusta formatação

--- app/load_tb_brewery_by_location.py
+++ app/load_tb_brewery_by_location.py
@@ -1,3 +1,6 @@
+import datetime
+import logging
+
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
@@ -7,13 +10,17 @@ SILVER_LAYER_PATH = "lake/2_silver"
 TABLE_PATH = "lake/3_gold/tb_brewery_data"
 FORMAT = "parquet"
 
-def run():
+
+def create_spark_session():
     conf = SparkConf().set("hive.exec.dynamic.partition", "true") \
         .set("hive.exec.dynamic.partition.mode", "nonstrict") \
         .set("spark.sql.sources.partitionOverwriteMode", "dynamic")
     spark = SparkSession.builder.config(
         conf=conf).enableHiveSupport().getOrCreate()
+    return spark
 
+
+def read_data_from_silver(spark):
     schema = StructType([
         StructField("id", StringType(), False),
         StructField("name", StringType(), False),
@@ -25,8 +32,8 @@ def run():
         StructField("state_province", StringType(), True),
         StructField("postal_code", StringType(), True),
         StructField("country", StringType(), True),
-        StructField("longitude", DoubleType(), True),  
-        StructField("latitude", DoubleType(), True),   
+        StructField("longitude", DoubleType(), True),
+        StructField("latitude", DoubleType(), True),
         StructField("phone", StringType(), True),
         StructField("website_url", StringType(), True),
         StructField("state", StringType(), True),
@@ -38,11 +45,33 @@ def run():
         .load(SILVER_LAYER_PATH)\
         .select("country", "brewery_type")
 
-    df_agg = df.groupBy("country", "brewery_type").count()
+    logging.info("Foram lidos %i registros da silver layer", df.count())
 
+    return df
+
+
+def create_aggregated_view(df):
+    df_agg = df.groupBy("country", "brewery_type").count()
+    logging.info("A view gerada contém %i registros", df_agg.count())
+    return df_agg
+
+
+def run():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info(f"Iniciando a execução do dia {datetime.date.today()}")
+    spark = create_spark_session()
+    df = read_data_from_silver(spark)
+    df_agg = create_aggregated_view(df)
+    logging.info("Salvando a view gerada...")
     df_agg.write\
-            .mode("overwrite") \
-            .format(FORMAT)\
-            .parquet(TABLE_PATH)
-            
+        .mode("overwrite") \
+        .format(FORMAT)\
+        .parquet(TABLE_PATH)
+    logging.info("Fim do processo.")
+
+    spark.stop()
+
+
 run()
