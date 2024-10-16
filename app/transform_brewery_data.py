c45fd6ba8ab918c13bec4a829261ba42fe6f9825
Rodrigo Pasquale <digoppasquale@msn.com>
2024-10-16 15:06:48 -0300

    Refatora módulo de transformação dos dados

--- app/transform_brewery_data.py
+++ app/transform_brewery_data.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime, timedelta
+import datetime
 
 
 from pyspark import SparkConf
@@ -11,13 +11,17 @@ BRONZE_LAYER_PATH = "lake/1_bronze"
 FORMAT = 'parquet'
 SILVER_LAYER_PATH = "lake/2_silver/tb_brewery_data"
 
-def run():
+date_now = datetime.date.today()
+
+def create_spark_session():
     conf = SparkConf().set("hive.exec.dynamic.partition", "true") \
         .set("hive.exec.dynamic.partition.mode", "nonstrict") \
         .set("spark.sql.sources.partitionOverwriteMode", "dynamic")
     spark = SparkSession.builder.config(
         conf=conf).enableHiveSupport().getOrCreate()
+    return spark
 
+def read_data_from_bronze(spark, file_path: str):
     schema = StructType([
         StructField("id", StringType(), False),
         StructField("name", StringType(), False),
@@ -41,14 +45,36 @@ def run():
     
     df = spark.read.schema(schema)\
                    .option('multiline', 'true')\
-                   .json(f'{BRONZE_LAYER_PATH}/*.json')
+                   .json(file_path)
+    return df
 
-
-    logging.info("Salvando %i registros na tb_brewery_data", df.count())
+def run():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info(f"Iniciando a execução do dia {date_now}")
+    spark = create_spark_session()
     
-    df.write.mode('overwrite') \
+    bronze_path = f'{BRONZE_LAYER_PATH}/*.json'
+    df_bronze = read_data_from_bronze(spark, bronze_path)
+    
+    logging.info("Foram lidos %i registros da camada bronze", df_bronze.count())
+    
+    # removendo registros que podem ter país com valor nulo ou tipo de brewery nulo, removendo ids duplicados
+    df_clean = df_bronze.filter((df_bronze.country.isNotNull()) & (df_bronze.brewery_type.isNotNull()))\
+                        .drop_duplicates(["id"])\
+                        .withColumn("country", trim(col("country"))) # existe um caso para United States que está escrito com um espaço a mais
+
+    logging.info("Dados limpos, %i registros", df_clean.count())
+    
+    logging.info("Escrevendo %i registros na silver layer", df_clean.count())
+
+    # repartition para melhorar a escrita dos arquivos por conta de small files
+    df_clean.repartition("country")\
+            .write.mode('overwrite') \
             .format(FORMAT)\
             .partitionBy('country') \
             .parquet(SILVER_LAYER_PATH)
-            
+    
+    logging.info("Fim do processo.")
+    
+    spark.stop()
 run()
