f22ef111f318d023926fca6c9f79b9a80e0d3a32
Rodrigo Pasquale <digoppasquale@msn.com>
2024-10-21 17:44:46 -0300

    Adiciona default_args e retry

--- dags/brewery_pipeline.py
+++ dags/brewery_pipeline.py
@@ -1,41 +1,53 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+from airflow.operators.bash import BashOperator
+from airflow.operators.email import EmailOperator
+from datetime import datetime, timedelta
 
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
 
 from dags.modules.extract_brewery_data import run as extract_data
 from dags.modules.transform_brewery_data import run as transform_data
 from dags.modules.load_tb_brewery_by_location import run as load_data
 
+default_args = {
+    'depends_on_past': False,
+    'start_date': datetime(2024, 10, 16),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=30)
+}
+
 dag = DAG(
     'bees_pipeline',
-    start_date=datetime(2024, 10, 16),
+    default_args=default_args,
+    schedule_interval='@daily',
     description='Pipeline para o case de DE da BEES',
+    default_view='graph',
     catchup=False
 )
 
 extract_task = PythonOperator(
     task_id='extract_data',
     python_callable=extract_data,
-    retries=3,
+    execution_timeout=timedelta(minutes=30),
     dag=dag,
 )
 
 transform_task = PythonOperator(
     task_id='transform_data',
     python_callable=transform_data,
-    retries=3,
+    execution_timeout=timedelta(minutes=30),
     dag=dag,
 )
 
 load_task = PythonOperator(
     task_id='load_data',
     python_callable=load_data,
-    retries=3,
+    execution_timeout=timedelta(minutes=30),
     dag=dag,
 )
 
