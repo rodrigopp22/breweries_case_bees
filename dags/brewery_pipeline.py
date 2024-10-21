3ccc764abd57cb89d236bfe6f437fa2b1412b1b6
Rodrigo Pasquale <digoppasquale@msn.com>
2024-10-21 17:45:52 -0300

    Remove import desnecessário

--- dags/brewery_pipeline.py
+++ dags/brewery_pipeline.py
@@ -1,7 +1,5 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
-from airflow.operators.email import EmailOperator
 from datetime import datetime, timedelta
 
 import os
